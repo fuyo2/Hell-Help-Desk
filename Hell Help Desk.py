994814d1d424d168234b27110c709823e0bcdfbc
fuyo2 <hilliardcole@gmail.com>
2018-10-19 16:25:36 -0400

    Formatting Changes

--- Hell Help Desk.py
+++ Hell Help Desk.py
@@ -12,26 +12,28 @@ import tkinter
 
 root = Tk()
 # Window title
-root.title ("Hell Help Desk")
+root.title("Hell Help Desk")
 # Window size.
 root.geometry("640x480")
 # Window icon.
 root.iconbitmap('favicon.ico')
-#root.configure(background='')
+# root.configure(background='')
 
 today = datetime.date.today()
 
+
 def retrieveInput():
     inputName = nameEntry.get()
-    print (inputName)
+#    print(inputName)
+
 
 def saveInput():
-    #Collects user input from text boxes and makes it a veriable
+    # Collects user input from text boxes and makes it a veriable
     inputName = nameEntry.get()
     inputDept = deptEntry.get()
     inputDescpt = descptEntry.get()
-    
-    #Opens and writes values of text boxes to a CSV file.
+
+    # Opens and writes values of text boxes to a CSV file.
     with open("output_data.csv", "a") as outFile:
         outString = str(inputName)
         outString += " , " + str(inputDept)
@@ -40,55 +42,60 @@ def saveInput():
         outString += "\n"
         outFile.write(outString)
 
-# About window under Help menu.    
+# About window under Help menu.
+
+
 def aboutWindow():
     root = Tk()
     # Window title
-    root.title ("About - Hell Help Desk")
+    root.title("About - Hell Help Desk")
     # Window size.
     root.geometry("320x210")
     # Window icon.
     root.iconbitmap('favicon.ico')
-    
+
     app = Frame(root)
     app.grid()
     app.place(relx=.5, rely=.5, anchor="center")
-    
+
     # Text label
-    topLabel = Label(app, text="Made by:",font=12)
+    topLabel = Label(app, text="Made by:", font=12)
     topLabel.grid(row=2, column=1, padx=5, pady=5)
-    
-    dateLabel = Label(app, text="Justin Cole 2018",font=12)
+
+    dateLabel = Label(app, text="Justin Cole 2018", font=12)
     dateLabel.grid(row=3, column=1, padx=5, pady=5)
-    
-    versionLabel = Label(app, text="version 0.03",font=12)
+
+    versionLabel = Label(app, text="version 0.03", font=12)
     versionLabel.grid(row=4, column=1, padx=5, pady=5)
-    
+
     # Quits out of the About window
-    okButton = Button(app, font=12, text="Ok", fg="black", command=root.destroy, height = 1, width = 20,)
+    okButton = Button(app, font=12, text="Ok", fg="black",
+                      command=root.destroy, height=1, width=20,)
     okButton.grid(row=10, column=1, padx=5, pady=5)
-    
+
     # Keeps the window in a loop to not close until okButton in pressed.
     root.mainloop()
 
 # Modify Existing Case window
+
+
 def modifyWindow():
     root = Tk()
     # Window title
-    root.title ("Modify Existing Case - Hell Help Desk")
+    root.title("Modify Existing Case - Hell Help Desk")
     # Window size.
     root.geometry("472x640")
     # Window icon.
     root.iconbitmap('favicon.ico')
-    
+
     app = Frame(root)
     app.grid()
     app.place(relx=.5, rely=.5, anchor="center")
-    
+
 #    topLabel = Label(app, text="Open Cases",font=12)
 #    topLabel.grid(row=1, column=1, padx=5, pady=5)
-    
-    with open("output_data.csv", newline = "") as file:
+
+    with open("output_data.csv", newline="") as file:
         reader = csv.reader(file)
 
         # r and c tell us where to grid the labels
@@ -96,16 +103,18 @@ def modifyWindow():
         for col in reader:
             c = 0
             for row in col:
-            # i've added some styling
-                labelCSV = tkinter.Label(root, width = 16, height = 2, text = row, relief = tkinter.RIDGE)
-                labelCSV.grid(row = r, column = c)
+                # i've added some styling
+                labelCSV = tkinter.Label(root, width=16, height=2, text=row, relief=tkinter.RIDGE)
+                labelCSV.grid(row=r, column=c)
                 c += 1
             r += 1
-    
+
     # Quits out of the About window
-    okButton = Button(app, font=12, text="Close", fg="black", command=root.destroy, height = 1, width = 20,)
+    okButton = Button(app, font=12, text="Close", fg="black",
+                      command=root.destroy, height=1, width=20,)
     okButton.grid(row=10, column=1, padx=5, pady=5)
-    
+
+
 app = Frame(root)
 app.grid()
 app.place(relx=.5, rely=.5, anchor="center")
@@ -113,7 +122,7 @@ app.place(relx=.5, rely=.5, anchor="center")
 # Menu bar.
 menu = Menu(app.master)
 app.master.config(menu=menu)
-    
+
 file = Menu(menu)
 file.add_command(label='Save', command=saveInput)
 file.add_command(label='Exit', command=root.destroy)
@@ -134,46 +143,51 @@ render = ImageTk.PhotoImage(logo)
 
 img = Label(app, image=render)
 img.image = render
-img.place(x=0,y=0)
+img.place(x=0, y=0)
 img.grid(row=1, column=1, padx=5, pady=5)
 
 # Text boxes and labels for the text boxes.
-nameLabel = Label(app, text="Name:",font=12)
+nameLabel = Label(app, text="Name:", font=12)
 nameLabel.grid(row=2, column=1, padx=5, pady=5)
 
 nameEntry = Entry(app, font=12, width=20)
 nameEntry.grid(row=3, column=1, padx=5, pady=5)
 
-deptLabel = Label(app, text="Department:",font=12)
+deptLabel = Label(app, text="Department:", font=12)
 deptLabel.grid(row=4, column=1, padx=1, pady=5)
 
 deptEntry = Entry(app, font=12, width=20)
 deptEntry.grid(row=5, column=1, padx=5, pady=5)
 
-descptLabel = Label(app, text="Description of issue:",font=12)
+descptLabel = Label(app, text="Description of issue:", font=12)
 descptLabel.grid(row=6, column=1, padx=1, pady=5)
 
 descptEntry = Entry(app, font=12, width=20)
 descptEntry.grid(row=7, column=1, padx=5, pady=5)
 
+
 def dialog():
     saveInput()
-    var = messagebox.showinfo("Case Submitted" , "Your ticket has been successfully submitted, It will never be reviewed.")
+    var = messagebox.showinfo(
+        "Case Submitted", "Your ticket has been successfully submitted, It will never be reviewed.")
+
 
 # This is the subit button.
-submitButton = Button(app, font=12, text = "Submit Ticket", height = 1, width = 20, command=dialog)
+submitButton = Button(app, font=12, text="Submit Ticket", height=1, width=20, command=dialog)
 submitButton.grid(row=8, column=1, padx=5, pady=5)
 
+
 def dialog2():
-    var = messagebox.showinfo("Case Submitted" , "You will never be able to modify your case.")
+    var = messagebox.showinfo("Case Submitted", "You will never be able to modify your case.")
+
 
 # This is the Modify Existing Case button
-ExCaseButton = Button(app, font=12, height = 1, width = 20)
+ExCaseButton = Button(app, font=12, height=1, width=20)
 ExCaseButton.grid(row=9, column=1, padx=5, pady=5)
-ExCaseButton.configure(text = "Modify Existing Case", command=modifyWindow)
+ExCaseButton.configure(text="Modify Existing Case", command=modifyWindow)
 
 # This is the QUIT button.
-quitButton = Button(app, font=12, text="QUIT", fg="red", command=root.destroy, height = 1, width = 20,)
+quitButton = Button(app, font=12, text="QUIT", fg="red", command=root.destroy, height=1, width=20,)
 quitButton.grid(row=10, column=1, padx=5, pady=5)
 
 # Keeps the program running in a loop until it is terminated by the user.
